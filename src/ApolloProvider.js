f9324e73b0e0164885c520a861f99cae3a392c38
teduri90 <teturi90@gmail.com>
2021-03-15 03:13:23 +0900

    cha

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: ' https://still-lake-12021.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
